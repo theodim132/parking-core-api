6f6f85a21ddfa90de9ddad7325ff2fb5e17a69c9
Thodoris Dimopoulos <tdimopoulos@vivawallet.com>
2026-01-18 19:49:40 +0200

    Fix logout page model build error

--- Parking.CoreApi/Pages/Account/Logout.cshtml.cs
+++ Parking.CoreApi/Pages/Account/Logout.cshtml.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Parking.CoreApi.Pages.Account;
